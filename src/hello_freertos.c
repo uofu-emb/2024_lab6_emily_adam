60386b576f00b656f423bcd050bad9dfb3e02678
Adam Blakeslee <adam.blakeslee@utah.edu>
2024-11-04 12:40:21 -0700

    made two threads in hello_freertos.c

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -8,7 +8,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-
+#include <semphr.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
@@ -17,11 +17,24 @@ int count = 0;
 bool on = false;
 
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
-#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
+#define HIGH_TASK_PRIORITY     ( tskIDLE_PRIORITY + 3UL )
+#define LOW_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define HIGH_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define LOW_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-void blink_task(__unused void *params) {
+
+
+void high_task(__unused void *params) {
+    hard_assert(cyw43_arch_init() == PICO_OK);
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+        if (count++ % 11) on = !on;
+        vTaskDelay(500);
+    }
+}
+
+void low_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
@@ -31,8 +44,11 @@ void blink_task(__unused void *params) {
 }
 
 void main_task(__unused void *params) {
-    xTaskCreate(blink_task, "BlinkThread",
-                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+    xTaskCreate(high_task, "HighThread",
+                HIGH_TASK_STACK_SIZE, NULL, HIGH_TASK_PRIORITY, NULL);
+    xTaskCreate(low_task, "LowThread",
+                LOW_TASK_STACK_SIZE, NULL, LOW_TASK_PRIORITY, NULL);
+
     char c;
     while(c = getchar()) {
         if (c <= 'z' && c >= 'a') putchar(c - 32);
@@ -46,6 +62,8 @@ int main( void )
     stdio_init_all();
     const char *rtos_name;
     rtos_name = "FreeRTOS";
+    SemaphoreHandle_t xSemaphore;
+    semaphore = xSemaphoreCreateBoolean();
     TaskHandle_t task;
     xTaskCreate(main_task, "MainThread",
                 MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
